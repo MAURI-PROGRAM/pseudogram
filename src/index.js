23c96e445b2077477f435d7759012e33f3c6cf07
unknown <merchan.ber.mau@gmail.com>
2019-03-30 11:09:02 -0500

    iniciando el proyecto e integrandolo con firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import firebase from 'firebase';
+// import {configKeyFirebase} from './Keys.js'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDPt4atywY8WijB6AuujuGFlNH3mMPw18Q",
+    authDomain: "pseudogram-f3c43.firebaseapp.com",
+    databaseURL: "https://pseudogram-f3c43.firebaseio.com",
+    projectId: "pseudogram-f3c43",
+    storageBucket: "pseudogram-f3c43.appspot.com",
+    messagingSenderId: "412609758018"
+  });
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
